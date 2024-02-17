415b464ff7a544bdd71a68f4293bb7f9b5d49597
MilanGohel <milan.gohel66@gmail.com>
2024-02-17 12:51:46 +0530

    first

--- script.js
+++ script.js
@@ -29,18 +29,50 @@
 
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
+const sign = document.querySelector("#operator")
+display.innerText = "0";
+let firstNumber = 0;
+let secondNumber = 0;
+let operation = "";
 let memory = 0;
 
 buttons.forEach((item) => {
   item.onclick = () => {
-    if (item.id == "clear") {
+    console.log(memory);
+    if(item.id === "-" || item.id === "+" || item.id === "*" || item.id === "/"){
+      sign.innerText = item.id;
+      let str = display.innerText.toString();
+      firstNumber = Number(str);
+      operation = item.id;
+      display.innerText = "0";
+      // display.innerText = "";
+    }
+    else if (item.id === "clear") {
       display.innerText = "";
-    } else if (item.id == "backspace") {
+    } else if (item.id == "backspace" && display.innerText !== "0") {
       let string = display.innerText.toString();
       display.innerText = string.substr(0, string.length - 1);
+    } else if (item.id == "backspace" && display.innerText === "0") {
+
     } else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
-    } else if (display.innerText == "" && item.id == "equal") {
+      secondNumber = Number(display.innerText);
+      switch(operation){
+        case "+":
+          display.innerText = firstNumber + secondNumber;
+          break;
+        case "-":
+          display.innerText = firstNumber - secondNumber;
+          break;
+        case "/":
+          display.innerText = firstNumber / secondNumber;
+          break;
+        case "*":
+          display.innerText = firstNumber * secondNumber;
+          break;
+      }
+      sign.innerText = "="
+      // display.innerText = firstNumber;
+    } else if (display.innerText === "" && item.id == "equal") {
       display.innerText = "Empty!";
       setTimeout(() => (display.innerText = ""), 2000);
     } else if(item.id === "M+" && display.innerText !== ""){
@@ -54,7 +86,11 @@ buttons.forEach((item) => {
     } else if(item.id === "MC"){
       memory = 0;
     } else {
-      display.innerText += item.id;
+      if(display.innerText !== "0" && item.id !== "0"){
+        display.innerText += item.id;
+      }
+      else if(display.innerText === "0" && item.id !== "0")
+        display.innerText = item.id;
     }
   };
 });
@@ -63,10 +99,12 @@ const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
 const toggleIcon = document.querySelector(".toggler-icon");
 const stringScreen = display.innerText.toString();
+
 let isDark = true;
 themeToggleBtn.onclick = () => {
   calculator.classList.toggle("dark");
   themeToggleBtn.classList.toggle("active");
   stringScreen.style.color = "white";
+  // sign.style.color = "white";
   isDark = !isDark;
 }
